06897f1a0dd950e35b6893d817921b2a923d3da0
Saul Ivan Rivas Vega <saul.ivan.rivas.vega@gmail.com>
2020-03-27 19:47:04 -0600

    latest changes

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,6 +16,16 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var stream;
+var nframes = 1024;
+var sample_rate = 44100.0;
+var freqs = [];
+for (var i = 0; i < nframes; ++i) {
+    freqs.push(0.0);
+}
+var counter = 0;
+var source;
+var toggleBtn = 0;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -28,43 +38,124 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        freqs[0] = 0.0;
+        for (var i = 1; i < nframes/2; ++i) {
+            freqs[i] = (i*sample_rate)/nframes;
+            freqs[nframes-i] = -freqs[i];
+        }
+        freqs[nframes/2] = ((nframes/2)*sample_rate)/nframes;
+
+        var audioCtx = undefined;
+        var getUserMedia;
+        var analyser;
+        var gainNode;
         document.querySelector('button').addEventListener('click', function() {
-            var AudioContext = window.AudioContext || window.webkitAudioContext;
-            var audioCtx = new AudioContext();
-          
-            // create Oscillator and gain node
-            var oscillator = audioCtx.createOscillator();
-            var gainNode = audioCtx.createGain();
-          
-            // connect oscillator to gain node to speakers
-          
-            oscillator.connect(gainNode);
-            gainNode.connect(audioCtx.destination);
-          
-            // create initial theremin frequency and volumn values
-          
-            var WIDTH = window.innerWidth;
-            var HEIGHT = window.innerHeight;
-          
-            var maxFreq = 6000;
-            var maxVol = 0.02;
-          
-            var initialFreq = 440;
-            var initialVol = 0.015;
-          
-            // set options for the oscillator
-          
-          
-            oscillator.detune.value = 100; // value in cents
-            oscillator.start(0);
-          
-            oscillator.onended = function() {
-              console.log('Your tone has now stopped playing!');
-            };
-          
-            gainNode.gain.value = initialVol;
-            gainNode.gain.minValue = initialVol;
-            gainNode.gain.maxValue = initialVol;
+
+            
+            
+            
+            
+            if (typeof audioCtx == "undefined") { 
+                audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+                // Some browsers partially implement mediaDevices. We can't just assign an object
+                // with getUserMedia as it would overwrite existing properties.
+                // Here, we will just add the getUserMedia property if it's missing.
+                if (navigator.mediaDevices.getUserMedia === undefined) {
+                    navigator.mediaDevices.getUserMedia = function(constraints) {
+
+                    // First get ahold of the legacy getUserMedia, if present
+                    getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+                    // Some browsers just don't implement it - return a rejected promise with an error
+                    // to keep a consistent interface
+                    if (!getUserMedia) {
+                        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                    }
+
+                    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+                    return new Promise(function(resolve, reject) {
+                        getUserMedia.call(navigator, constraints, resolve, reject);
+                    });
+                    }
+                }
+                
+                analyser = audioCtx.createAnalyser();
+                analyser.fftSize = 1024;
+                gainNode = audioCtx.createGain();
+            }
+
+            if (toggleBtn == 0) {
+                toggleBtn = 1;
+                gainNode.gain.setTargetAtTime(1, audioCtx.currentTime, 0);
+            } else {
+                toggleBtn = 0;
+                gainNode.gain.setTargetAtTime(0, audioCtx.currentTime, 0);
+                // window.cancelAnimationFrame(analyzeSignal);
+                return;
+            }
+
+            
+
+            if (navigator.mediaDevices.getUserMedia) {
+                console.log('getUserMedia supported.');
+                var constraints = {audio: true}
+                navigator.mediaDevices.getUserMedia (constraints)
+                   .then(
+                     function(stream) {
+                        source = audioCtx.createMediaStreamSource(stream);
+                        source.connect(gainNode);
+                        gainNode.connect(analyser);
+                        analyser.connect(audioCtx.destination);
+                        
+                        function analyzeSignal() {
+                            if (toggleBtn ==1) 
+                            requestAnimationFrame(analyzeSignal);
+                            
+                            // console.log("++++++++++++++++++++++++++++++++++");
+                            // Get Signal values
+                            // analyser.fftSize = 2048;
+                            // var bufferLength = analyser.fftSize;
+                            // var dataArray = new Uint8Array(bufferLength);
+                            // analyser.getByteTimeDomainData(dataArray);
+                            // console.log("SIGNAL:")
+                            // console.log(bufferLength);
+                            // console.log(dataArray);
+                            // console.log("FFT:");
+                            
+                            var bufferLengthAlt = analyser.frequencyBinCount;
+                            var dataArrayAlt = new Uint8Array(bufferLengthAlt);
+                            analyser.getByteFrequencyData(dataArrayAlt);
+                            // console.log(bufferLengthAlt);
+                            var max_values = [];
+                            
+                            for (var i = 0; i < 512; ++i) {
+
+                                // console.log("FREQ(" + freqs[i] + "): " + dataArrayAlt[i]);
+                                max_values.push({"Freq":freqs[i], "value": dataArrayAlt[i]});
+
+                            }
+                            max_values.sort(function(a,b){
+                                return a.value - b.value;
+                            });
+                            // console.log(max_values);
+                            var signal_values_text = document.getElementById("signal_values_text");
+                            var stringChanges = "TOP 3 Frequencies \n";
+                            stringChanges += "Freq: " + max_values[511].Freq + " Value : " + max_values[511].value + "\n";
+                            stringChanges += "Freq: " + max_values[510].Freq + " Value : " + max_values[510].value + "\n";
+                            stringChanges += "Freq: " + max_values[509].Freq + " Value : " + max_values[509].value + "\n";
+                            signal_values_text.innerText = stringChanges;
+                        }
+                        
+                        requestAnimationFrame(analyzeSignal);
+
+
+
+                   })
+                   .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+             } else {
+                console.log('getUserMedia not supported on your browser!');
+             }
+        
           
         });
     },
